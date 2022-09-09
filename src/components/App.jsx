b1be6f5bd6364249d53c496462e14b7aa267a675
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-09-09 22:35:21 +0300

    add Task-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+// import data from '../user.json'
+import { Profile } from './Profile/Profile.jsx'
+
+const data = {
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308
+  }
+}
+console.log(data)
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile user={data}/>
     </div>
   );
 };
+
