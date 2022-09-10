62c72ccc46c1c3cb31281f9e2c8f36c3481db520
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-09-10 18:44:40 +0300

    Task-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,18 @@
 import user from '../user.json'
 import data from '../data.json'
+import friends from '../friends.json'
 import { Profile } from './Profile/Profile.jsx'
 import { Statistics } from './Statistics/Statistics.jsx'
+import { FriendsList } from './FriendList/FriendList'
 
 
-// console.log(data)
+
 export const App = () => {
   return (
     <div>
       <Profile user={user} />
-      <Statistics stats={data} title = {"Upload stats"}/>
+      <Statistics stats={data} title={"Upload stats"} />
+      <FriendsList friends={friends} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,28 @@
+import styles from "./friendList.module.css"
+import PropTypes from "prop-types";
+
+const FriendsList = ({ friends }) => {
+    console.log(friends[1].name)
+    return (
+        <ul className={styles.friendList}>
+            {friends.map(item => (
+                <li className={styles.item} key={item.id}>
+                    {item.isOnline? <span className={styles.isOnline}></span>:<span className={styles.isOfline}></span>}
+                    <img className={styles.avatar} src={item.avatar} alt="User avatar" width="48" />
+                    <p className={styles.name}>{item.name}</p>
+                </li>)
+            )}
+        </ul>)
+}   
+
+
+export { FriendsList }
+
+FriendsList.propTypes = {
+    friends: PropTypes.arrayOf(PropTypes.shape({
+        avatar: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        isOnline: PropTypes.bool.isRequired,
+        id: PropTypes.number.isRequired,
+    })),
+}
