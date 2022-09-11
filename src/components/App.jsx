32e21ca9a5d41f86d6ffd861549e8d8cae5fd0c5
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-09-11 22:12:26 +0300

    Task-4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import user from '../user.json'
 import data from '../data.json'
 import friends from '../friends.json'
+import transactions from '../transactions.json'
 import { Profile } from './Profile/Profile'
 import { Statistics } from './Statistics/Statistics'
 import { FriendsList } from './FriendList/FriendList'
@@ -14,7 +15,7 @@ export const App = () => {
       <Profile user={user} />
       <Statistics stats={data} title={"Upload stats"} />
       <FriendsList friends={friends} />
-      <TransactionHistory/>
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,29 +1,24 @@
 import styles from "./transactionHistory.module.css"
 import PropTypes from "prop-types";
 
-const TransactionHistory = () => {
-    console.log('Hi') 
+const TransactionHistory = ({ items }) => { 
     return (
-        <table class="transaction-history">
-            <thead>
+        <table className={styles.transactionHistory}>
+            <thead className={styles.tableHead}>
                 <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
+                    <th className={styles.cell}>Type</th>
+                    <th className={styles.cell}>Amount</th>
+                    <th className={styles.cell}>Currency</th>
                 </tr>
             </thead>
-
             <tbody>
-                <tr>
-                    <td>Invoice</td>
-                    <td>125</td>
-                    <td>USD</td>
-                </tr>
-                <tr>
-                    <td>Withdrawal</td>
-                    <td>85</td>
-                    <td>USD</td>
-                </tr>
+                {items.map(item => (
+                    <tr className={styles.tableRow} key = {item.id}>
+                        <td className={styles.cell}>{item.type}</td>
+                        <td className={styles.cell}>{item.amount}</td>
+                        <td className={styles.cell}>{item.currency}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     )
